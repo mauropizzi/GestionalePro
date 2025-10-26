0501f48c60946f64160ea40a30558f19b2bc089e
mauropizzi <m.pizzi.bs@gmail.com>
2025-10-26 12:18:13 +0100

    Reverted all changes back to version 41c4eb3d9435a4289de1acf5e05a374841e10dc3

--- src/app/richieste-servizio/[id]/edit/page.tsx
+++ src/app/richieste-servizio/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import DashboardLayout from "@/components/dashboard-layout";
 import { Loader2, ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
