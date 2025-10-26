922b81ae660033dd4add38963607601bd079314a
mauropizzi <m.pizzi.bs@gmail.com>
2025-10-26 12:17:09 +0100

    [dyad] Resa più compatta la pagina di modifica richiesta di servizio - wrote 1 file(s)

--- src/app/richieste-servizio/[id]/edit/page.tsx
+++ src/app/richieste-servizio/[id]/edit/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import DashboardLayout from "@/components/dashboard-layout";
 import { Loader2, ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
