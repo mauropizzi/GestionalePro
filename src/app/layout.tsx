dfe5e178442a4d5d5ae3e6222fdf14cb2c5785fc
mauropizzi <m.pizzi.bs@gmail.com>
2025-10-27 13:45:48 +0100

    Reverted all changes back to version 5f6521a7f70f6c5e53c3b42532678b611be5cc54

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SessionContextProvider } from "@/components/session-context-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "next-themes"; // Import ThemeProvider
 
 export const metadata: Metadata = {
   title: "Gestione Accessi App",
@@ -12,9 +15,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="it">
-      <body>
-        {children}
+    <html lang="it" suppressHydrationWarning>
+      <body
+        className={`font-sans antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SessionContextProvider>
+            {children}
+          </SessionContextProvider>
+        </ThemeProvider>
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
